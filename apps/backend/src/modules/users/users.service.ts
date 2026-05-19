96c25c05bdaf292a836879ceaab350f00df33179
SNOWNUMB <mamayev.stas@gmail.com>
2026-05-19 14:43:40 +0500

    chore: обновление структуры изменений кода

--- apps/backend/src/modules/users/users.service.ts
+++ apps/backend/src/modules/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { DatabaseService } from '@/core/database/database.module';
+import { DatabaseService } from '../../core/database/database.module';
 
 @Injectable()
 export class UsersService {
